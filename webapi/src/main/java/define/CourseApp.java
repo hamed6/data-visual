6b901cd848482e2b9be20855f100d04bd3705b84
hamed6 <h.ghodsinezhad@gmail.com>
2021-11-07 10:02:51 +0200

    rebuild spring

---
+++ webapi/src/main/java/define/CourseApp.java
@@ -0,0 +1,12 @@
+package define;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApp {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
--- webapi/src/main/java/topic/TopicService.java
+++ webapi/src/main/java/topic/TopicService.java
@@ -1,5 +1,9 @@
 package topic;
 
+
+import org.springframework.stereotype.Service;
+
+@Service
 public class TopicService {
 
 }
