1b9101feac5bc0781e1450687d8475c8164cd393
hamed6 <h.ghodsinezhad@gmail.com>
2021-11-11 15:02:07 +0200

    update JPA and crud

--- webapi/src/main/java/define/controller/Topic.java
+++ webapi/src/main/java/define/controller/Topic.java
@@ -1,7 +1,12 @@
 package define.controller;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Topic {
 	
+	@Id
 	private String id;
 	private String description;
 	
--- webapi/src/main/java/define/controller/TopicService.java
+++ webapi/src/main/java/define/controller/TopicService.java
@@ -4,10 +4,14 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TopicService {
+	
+	@Autowired
+	private TopicRepository topicRepository; 
 	
 	private List<Topic> topics= new ArrayList<>( Arrays.asList
 			(
@@ -16,6 +20,9 @@ public class TopicService {
 			));
 	
 	public List<Topic> getAllTopics(){
+//		return topics;
+		List <Topic> topics= new ArrayList<>();
+		topicRepository.findAll().forEach(topics::add);
 		return topics;
 	}
 	
@@ -24,8 +31,8 @@ public class TopicService {
 	}
 
 	public void addTopic(Topic topic) {
-		topics.add(topic);
-		
+//		topics.add(topic);
+		topicRepository.save(topic);
 	}
 
 	public void updateTopic(Topic topic, String id) {
