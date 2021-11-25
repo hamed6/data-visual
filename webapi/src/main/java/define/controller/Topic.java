1840026ae90433be7ebd018c1108e63a0447455f
hamed6 <h.ghodsinezhad@gmail.com>
2021-11-25 13:25:07 +0200

    add mysql db properties

--- webapi/src/main/java/define/controller/Topic.java
+++ webapi/src/main/java/define/controller/Topic.java
@@ -1,12 +1,15 @@
 package define.controller;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Topic {
 	
 	@Id
+//	@GeneratedValue(strategy=GenerationType.AUTO)
 	private String id;
 	private String description;
 	
