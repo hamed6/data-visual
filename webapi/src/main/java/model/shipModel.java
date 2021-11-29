96948d5508adee6e87ee8389bb87aa9e31d560df
hamed6 <h.ghodsinezhad@gmail.com>
2021-11-29 15:31:12 +0200

    model

---
+++ webapi/src/main/java/model/shipModel.java
@@ -0,0 +1,32 @@
+package model;
+
+import java.sql.Date;
+
+import javax.persistence.Entity;
+
+@Entity
+public class shipModel {
+	
+	// Unique, index, 
+	private int id;
+	
+	// Ship info specific 
+	private int imo;
+	private String shipName;
+	
+	// Log files details
+	private Date logTime;
+	private String logClass;
+	private String logDescrption;
+	private String logMore;
+	
+	// panel ID extracted from log
+	private String logHCID;
+	
+	
+/*	# To get and convert date and time
+  	java.util.Date dt = new java.util.Date();
+	java.text.SimpleDateFormat sdf = new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+	String currentTime = sdf.format(dt);
+*/
+}
