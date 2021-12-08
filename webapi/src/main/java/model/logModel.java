6aa82f1766c3748efb3ab2a20bc53d0fb2105f31
hamed6 <h.ghodsinezhad@gmail.com>
2021-12-08 12:11:24 +0200

    model

--- webapi/src/main/java/model/logModel.java
+++ webapi/src/main/java/model/logModel.java
@@ -1,8 +1,84 @@
 package model;
 
+import java.sql.Date;
+
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
 public class logModel {
 
+
+	//IMO column shouldn't be empty and updatable
+	//@Column(nullable = false, updatable = false)
+	@Id
+	private int imo;
+	// *** panel ID and filename extracted from log
+	//private String logHCID;
+	private String logFilename;
+
+	// Log files details
+	private Date logTime;
+	private String logClass;
+	private String logDescrption;
+	private String logMore;
+	
+	
+	
+	
+	public logModel() {
+	}
+	
+	
+	
+	public logModel(int imo, String logFilename, Date logTime, String logClass, String logDescrption, String logMore) {
+		super();
+		this.imo = imo;
+		this.logFilename = logFilename;
+		this.logTime = logTime;
+		this.logClass = logClass;
+		this.logDescrption = logDescrption;
+		this.logMore = logMore;
+	}
+
+
+
+	public int getImo() {
+		return imo;
+	}
+	public void setImo(int imo) {
+		this.imo = imo;
+	}
+	public String getLogFilename() {
+		return logFilename;
+	}
+	public void setLogFilename(String logFilename) {
+		this.logFilename = logFilename;
+	}
+	public Date getLogTime() {
+		return logTime;
+	}
+	public void setLogTime(Date logTime) {
+		this.logTime = logTime;
+	}
+	public String getLogClass() {
+		return logClass;
+	}
+	public void setLogClass(String logClass) {
+		this.logClass = logClass;
+	}
+	public String getLogDescrption() {
+		return logDescrption;
+	}
+	public void setLogDescrption(String logDescrption) {
+		this.logDescrption = logDescrption;
+	}
+	public String getLogMore() {
+		return logMore;
+	}
+	public void setLogMore(String logMore) {
+		this.logMore = logMore;
+	}
+	
+	
 }
--- webapi/src/main/java/model/shipModel.java
+++ webapi/src/main/java/model/shipModel.java
@@ -14,104 +14,57 @@ import javax.persistence.Table;
 @Table(name="shipModel")
 public class shipModel implements Serializable { 
 	
-	@Id
+	
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
 	
 	// Ship info specific
 	//IMO column shouldn't be empty and updatable
-		//@Column(nullable = false, updatable = false)
+	//@Column(nullable = false, updatable = false)
+	@Id
 	private int imo;
 	private String shipName;
-	
-	// Log files details
-	private Date logTime;
-	private String logClass;
-	private String logDescrption;
-	private String logMore;
-	
-	// *** panel ID and filename extracted from log
-	//private String logHCID;
-	private String logFilename;
-	
+
+
 	public shipModel() {
-		
 	}
-	public shipModel(int id, int imo, String shipName, Date logTime, String logClass, String logDescrption,
-			String logMore, String logFilename) {
+	
+
+	public shipModel(int id, int imo, String shipName) {
 		super();
 		this.id = id;
 		this.imo = imo;
 		this.shipName = shipName;
-		this.logTime = logTime;
-		this.logClass = logClass;
-		this.logDescrption = logDescrption;
-		this.logMore = logMore;
-		this.logFilename = logFilename;
 	}
+
 
 	public int getId() {
 		return id;
 	}
 
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 
 	public int getImo() {
 		return imo;
 	}
 
+
 	public void setImo(int imo) {
 		this.imo = imo;
 	}
+
 
 	public String getShipName() {
 		return shipName;
 	}
 
+
 	public void setShipName(String shipName) {
 		this.shipName = shipName;
-	}
-
-	public Date getLogTime() {
-		return logTime;
-	}
-
-	public void setLogTime(Date logTime) {
-		this.logTime = logTime;
-	}
-
-	public String getLogClass() {
-		return logClass;
-	}
-
-	public void setLogClass(String logClass) {
-		this.logClass = logClass;
-	}
-
-	public String getLogDescrption() {
-		return logDescrption;
-	}
-
-	public void setLogDescrption(String logDescrption) {
-		this.logDescrption = logDescrption;
-	}
-
-	public String getLogMore() {
-		return logMore;
-	}
-
-	public void setLogMore(String logMore) {
-		this.logMore = logMore;
-	}
-
-	public String getLogFilename() {
-		return logFilename;
-	}
-
-	public void setLogFilename(String logFilename) {
-		this.logFilename = logFilename;
 	}
 
 	
