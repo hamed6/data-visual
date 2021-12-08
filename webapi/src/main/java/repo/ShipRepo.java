90c3228ec583bf518121ffc990e83bb7f8ce2b3d
hamed6 <h.ghodsinezhad@gmail.com>
2021-12-08 16:53:28 +0200

    model

--- webapi/src/main/java/repo/ShipRepo.java
+++ webapi/src/main/java/repo/ShipRepo.java
@@ -6,4 +6,11 @@ import model.shipModel;
 
 public interface ShipRepo extends JpaRepository<shipModel, Integer> {
 
+	
+
+	
+	// *Optional* if adding instead of shipModle, then in case that the ship doesnt exists, it wont complain.
+	//Optional findshipModelById(int imo);
+	shipModel findshipModelById(int imo);
+
 }
--- webapi/src/main/java/service/ShipService.java
+++ webapi/src/main/java/service/ShipService.java
@@ -1,8 +1,12 @@
 package service;
 
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import model.shipModel;
 import repo.ShipRepo;
 
 @Service
@@ -10,5 +14,20 @@ public class ShipService {
 	
 	@Autowired
 	private ShipRepo shipRepo;
-
+	
+	public shipModel addshipModel(shipModel shipmodel) {
+		return shipRepo.save(shipmodel);
+	}
+	
+	public shipModel updateshipModel(shipModel shipmodel) {
+		return shipRepo.save(shipmodel);
+	}
+	
+	public List<shipModel> findallshipModel(){
+		return shipRepo.findAll();
+	}
+	public shipModel findshipModelById(int imo) {
+		return shipRepo.findshipModelById(imo);
+//		return shipRepo.findshipModelById(imo).orElseThrow(()) -> new UserPrincipalNotFoundException("this ship doenst exist"));
+	}
 }
